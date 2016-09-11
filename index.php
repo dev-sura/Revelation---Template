cdf723c0a7f10f40136b933cf72d9e8dd2c5ab7f
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-09-11 20:26:40 +0530

    1

--- index.php
+++ index.php
@@ -1861,7 +1861,7 @@ $Name = $_GET['name'];
 				<div class="blog-item-wrapper">
 					<!-- blog item thumbnail -->
 					<div class="blog-item-thumb">
-						<a href="mt.php" class=""><img src="img/mt.jpg" alt=""></a>
+						<a href="mt.php" class=""><img style="height:260px;" src="img/mt.jpg" alt=""></a>
 					</div>
 					<!-- /blog item thumbnail -->
 					<!-- Blog item - infos -->
@@ -2169,7 +2169,7 @@ $Name = $_GET['name'];
 		</div>
 		<!-- /SECTION: Location
 		================================================== -->
-		
+
 				<!-- SECTION: Sponsors
 		================================================== -->
 		<div class="section-sponsors inverted-section2 section-padding " id="section-sponsors">
@@ -2214,7 +2214,7 @@ $Name = $_GET['name'];
 		</div>
 		<!-- /SECTION: Sponsors
 		================================================== -->
-		
+
 	</div>
 </div>
 
