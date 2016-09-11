7ea3b4276fb534deb0e1c87f40394eddf3a3fc5e
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-09-11 08:44:12 +0530

    1

--- Events.php
+++ Events.php
@@ -33,3 +33,39 @@
 	<!-- /post content -->
 </div>
 <!-- /Blog Posts -->
+
+<!-- Blog Posts -->
+<div id="sp" class="blog-single-post">
+	<!-- post image -->
+	<div class="post-header-image">
+		<img src="img/sp.jpg" style="height:300px;width:300px;margin-left:260px;" alt="">
+	</div>
+	<!-- /post-image -->
+
+	<!-- post title -->
+	<h2 class="blog-single-title">Spideys</h2>
+	<!-- /post title -->
+
+	<!-- post content -->
+	<div class="blog-single-content">
+		<p>Rules for Paper Presentation</p>
+		<ul>
+			<li>Only CS domains are encouraged.</li>
+			<li>Maximum of 3 authors and only student authors allowed.</li>
+			<li>We take plagiarism very seriously. Only original research works are accepted.</li>
+			<li>The paper must not have been presented or published anywhere else.</li>
+			<li>The paper must be in two column IEEE format. This format must be strictly adhered to. The paper length must be limited to 6 pages.</li>
+			<li>The paper must be in .pdf or .doc or .docx formats.</li>
+			<li>The paper must be sent through mail. The instructions are as follows:
+			<ul>
+				<li>The subject must be the title of the paper.</li>
+				<li>The body of the mail should include - Name and Institute of participant (starting from the first author and second one), Department and Year, Contact numbers,Email addresses.</li>
+				<li>Send the paper as an attachment to hitsrevelation@gmail.com</li>
+			</ul> </li>
+			<li>The Last date to send in the paper: 14th September 2016</li>
+			<li>Acceptance notification: 15th September 2016</li>
+		</ul>
+	</div>
+	<!-- /post content -->
+</div>
+<!-- /Blog Posts -->
